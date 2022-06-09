f0ac6022e76a3af93f12eaa5413246a2a2ad94d3
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2022-06-09 20:35:09 -0300

    Projeto para pt-br

---
+++ app/src/Jogos/Controller/IJogosController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\src\Jogos\Controller;
+
+use App\src\Jogos\Request\JogoRequest;
+use Illuminate\Http\JsonResponse;
+
+interface IJogosController
+{
+    public function store(JogoRequest $request): JsonResponse;
+}
