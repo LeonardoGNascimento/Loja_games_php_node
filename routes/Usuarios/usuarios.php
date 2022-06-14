61b6165cbdd9117165cd44cd8da7c3cf602fcf90
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2022-06-14 19:05:23 -0300

    configurando login

--- routes/Usuarios/usuarios.php
+++ routes/Usuarios/usuarios.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [UsuarioController::class, 'login'])->name('login');
 
 Route::prefix('usuarios')->group(function () {
-    Route::post('/', [UsuarioController::class, 'store'])->middleware('auth:sanctum');
+    Route::post('/', [UsuarioController::class, 'store']);
 });
